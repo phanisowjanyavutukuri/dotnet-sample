addcbf44159bbdf25794ebcf2b5bd6b7556ef975
megh-6789 <boggarapumeghana@gmail.com>
2017-09-29 18:17:01 +0530

    Update variables.tf

--- automation/Terraform/modules/ecs/variables.tf
+++ automation/Terraform/modules/ecs/variables.tf
@@ -1,22 +1,16 @@
- variable "ecs_ami" {
+  variable "ecs_ami" {
     default = "ami-1d668865"
   }
-
   variable "subnet_id" {
     default = "subnet-9e4762c7"
   }
   variable "security_group_id" {
     default = "sg-5ec56439"
   }
-   variable "key_name" {
-    default = "megh_key"
-  }
-  
   variable "key_name" {
     default = "megh_key"
   }
-
-
+  
   variable "instance_type" {
     default = "t2.micro"
   }
