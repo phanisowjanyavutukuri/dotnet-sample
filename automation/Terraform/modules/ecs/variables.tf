74a8ef8a2bd849bc990ddae321c6b60cf641b0e8
megh-6789 <boggarapumeghana@gmail.com>
2017-09-29 17:44:50 +0530

    Create variables.tf

---
+++ automation/Terraform/modules/ecs/variables.tf
@@ -0,0 +1,13 @@
+ variable "ecs_ami" {
+    default = "ami-1d668865"
+  }
+
+  variable "subnet_id" {
+    default = "subnet-9e4762c7"
+  }
+  variable "security_group_id" {
+    default = "sg-5ec56439"
+  }
+  variable "instance_type" {
+    default = "t2.micro"
+  }
