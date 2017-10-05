2dff4145eb88854736bbc3ebe0cb4a16565a61a8
megh-6789 <boggarapumeghana@gmail.com>
2017-10-05 14:39:41 +0530

    Update variables.tf

--- automation/Terraform/modules/ecs/variables.tf
+++ automation/Terraform/modules/ecs/variables.tf
@@ -1,6 +1,6 @@
   
   variable "key_name" {
-    default = "megh_key"
+    default = "meghana"
   }
 
   variable "instance_type" {
@@ -8,15 +8,7 @@
   }
 
   variable "ecs_aws_ami" {
-    default = "ami-1d668865"
+    default = "ami-34032e51"
   }
 
-  variable "ecs_config" {
-    default     = "echo '' > /etc/ecs/ecs.config"
-    description = "Specify ecs configuration or get it from S3. Example: aws s3 cp s3://some-bucket/ecs.config /etc/ecs/ecs.config"
-  } 
   
-  variable "ecs_logging" {
-    default     = "[\"json-file\",\"awslogs\"]"
-    description = "Adding logging option to ECS that the Docker containers can use. It is possible to add fluentd as well"
-  }
