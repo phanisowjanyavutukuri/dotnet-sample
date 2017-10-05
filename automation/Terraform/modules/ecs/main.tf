f92d2e138f82c3b423448afa4c64dfd648717b58
Ubuntu <ubuntu@ip-172-31-22-80.us-west-2.compute.internal>
2017-10-05 06:38:36 +0000

    resolved json

--- automation/Terraform/modules/ecs/main.tf
+++ automation/Terraform/modules/ecs/main.tf
@@ -8,15 +8,15 @@
     name = "megh-cluster"
   } 
  
-  resource "aws_ecs_task_definition" "config" {
-    family = "config"
+  resource "aws_ecs_task_definition" "test" {
+    family = "test"
     container_definitions = "${file("task-definitions/test.json")}"
   } 
  
   resource "aws_ecs_service" "test_service" {
     name            = "megh-service"
     cluster         = "${aws_ecs_cluster.cluster.id}"
-    task_definition = "${aws_ecs_task_definition.config.arn}"
+    task_definition = "${aws_ecs_task_definition.test.arn}"
     desired_count   = 1
   }
  
