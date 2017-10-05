80bc60668026efde62005483b158450802174fbe
megh-6789 <boggarapumeghana@gmail.com>
2017-10-05 14:23:25 +0530

    Update main.tf

--- automation/Terraform/modules/ecs/main.tf
+++ automation/Terraform/modules/ecs/main.tf
@@ -5,18 +5,18 @@
   
   
   resource "aws_ecs_cluster" "cluster" {
-    name = "megh-cluster"
+    name = "my-cluster"
   } 
  
-  resource "aws_ecs_task_definition" "test" {
-    family = "sample"
+  resource "aws_ecs_task_definition" "task_def" {
+    family = "my_task"
     container_definitions = "${file("task-definitions/test.json")}"
   } 
  
   resource "aws_ecs_service" "test_service" {
-    name            = "megh-service"
+    name            = "my-service"
     cluster         = "${aws_ecs_cluster.cluster.id}"
-    task_definition = "${aws_ecs_task_definition.test.arn}"
+    task_definition = "${aws_ecs_task_definition.task_def.arn}"
     desired_count   = 1
   }
  
